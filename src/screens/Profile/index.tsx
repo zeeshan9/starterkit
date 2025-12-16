2acd196b5487317b9e21a761818aff673ee12818
zeeshan mushtaq <zeeshanmushtaq76@gmail.com>
2025-12-16 10:05:42 +0500

    revert comment.

--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -4,7 +4,7 @@ import { Text, View } from 'react-native'
 const Profile = () => {
   return (
     <View>
-      {/* Profile Screen View */}
+      {/* Profile Screen */}
       <Text>Profile Screen</Text>
     </View>
   )
