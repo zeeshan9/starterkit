de9576363d4f345a2efa3aad0d2deebf55a39e8b
zeeshan mushtaq <zeeshanmushtaq76@gmail.com>
2026-01-05 19:39:53 +0500

    commit msg

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -6,7 +6,7 @@ import BottomNavigator from '../../navigation/BottomNavigator';
 const Home = () => {
   return (
     <View style={{flex:1}}>
-      {/* Two different Root scene components for UI feature */}
+      {/* Two different Root scene components for UI features */}
       <BottomNavigator />
       {/* <DrawerNavigator /> */}
     </View> 
